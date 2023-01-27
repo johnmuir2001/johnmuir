140b2ced78c7d637e73f32a690fdf48ed5827259
John Muir <johnmuir@CN0400.lan>
2023-01-27 13:51:38 +0000

    project card parallax on individual cards

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import styled from 'styled-components';
 import clouds from './assets/images/cloud.webp';
 import beach from './assets/images/beach.webp';
@@ -6,7 +5,6 @@ import { projects } from "./ProjectWork";
 import ProjectCard from "./components/ProjectCard";
 
 const App = () => {
-  const [backgroundPos, setBackgroundPos] = useState("50% 50%")
 
   return (
     <Parallax id="App" className="wrapper">
@@ -23,16 +21,7 @@ const App = () => {
           <ProjectCardWrapper>
             {projects.map((work, index) => {
               return(
-                <CardWrapper 
-                  key={index}
-                  image={work.image_main}
-                  imagePos={backgroundPos}
-                  className="card"
-                  onMouseMove={(e) => setBackgroundPos(`${(((e.clientX - e.target.offsetLeft) - (e.target.offsetWidth/2))/7) + 50}% ${(((e.clientY - e.target.getBoundingClientRect().y) - (e.target.offsetHeight/2))/4) + 50}%`)}
-                  onMouseOut={(e) => setBackgroundPos("50% 50%")}
-                >
-                  <h1>{work.name}</h1>
-                </CardWrapper>
+                <ProjectCard key={index} project={work}/>
               )
             })}
           </ProjectCardWrapper>
@@ -84,55 +73,8 @@ const ProjectCardWrapper = styled.div`
   flex-wrap: wrap;
   justify-content: center;
   width: 100%;
+  max-width: 2200px;
   background-color: red;
   box-sizing: border-box;
   padding: 50px;
-`;
-
-const CardWrapper = styled.div`
-    position: relative;
-    margin: 5px;
-    width: calc((100% - 50px) /3);
-    background-color: red;
-    background-size: auto 130%;
-    background-position: ${(props) => props.imagePos};
-    background-image: ${(props) => `url(${props.image})`};
-    transition: all 1s cubic-bezier(.24,1.04,.79,1);
-    overflow: hidden;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    aspect-ratio: 5/3;
-
-    &:hover {
-        background-size: auto 120%;
-    }
-
-    &::before {
-        position: absolute;
-        content: '';
-        height: 100%;
-        width: 100%;
-        background-color: rgba(0, 0, 0, 0);
-        transition: all 0.4s;
-    }
-
-    &:hover::before {
-        background-color: rgba(0, 0, 0, 0.2);
-    }
-
-    h1 {
-        color: white;
-        letter-spacing: 5px;
-        transition: all 0.4s;
-        opacity: 0;
-        filter: blur(5px);
-        pointer-events: none;
-    }
-
-    &:hover h1 {
-        letter-spacing: 1px;
-        opacity: 1;
-        filter: blur(0px);
-    }
 `;
